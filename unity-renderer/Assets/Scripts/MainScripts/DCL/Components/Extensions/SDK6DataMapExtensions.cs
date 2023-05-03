91ec4c82a8ea9a80e4c4f1c8d1d7b89e2961f44f
Vitaly Popuzin <popuzin@gmail.com>
2023-05-03 17:23:00 +0200

    adjusted extension

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Components/Extensions/SDK6DataMapExtensions.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Components/Extensions/SDK6DataMapExtensions.cs
@@ -1,21 +1,20 @@
 ﻿using DCL.Components;
-using Decentraland.Common;
 using UnityEngine;
 
 namespace MainScripts.DCL.Components
 {
     public static class SDK6DataMapExtensions
     {
-        public static Color AsUnityColor(this Color4 color4) =>
-            new (color4.R, color4.G, color4.B, color4.A);
+        public static Color AsUnityColor(this Decentraland.Sdk.Ecs6.ECS6Color4 color4) => 
+            new(color4.R, color4.G, color4.B, color4.HasA? color4.A : 1f);
 
-        public static Color AsUnityColor(this Color3 color3) =>
+        public static Color AsUnityColor(this Decentraland.Common.Color3 color3) =>
             new (color3.R, color3.G, color3.B);
 
-        public static UnityEngine.Vector3 AsUnityVector3(this Decentraland.Common.Vector3 vector3) =>
+        public static Vector3 AsUnityVector3(this Decentraland.Common.Vector3 vector3) =>
             new (vector3.X, vector3.Y, vector3.Z);
 
-        public static UnityEngine.Quaternion AsUnityQuaternion(this Decentraland.Common.Quaternion quaternion) =>
+        public static Quaternion AsUnityQuaternion(this Decentraland.Common.Quaternion quaternion) =>
             new (quaternion.X, quaternion.Y, quaternion.Z, quaternion.W);
         
         public static UIValue FromProtobuf(UIValue defaultValue, Decentraland.Sdk.Ecs6.UiValue uiValue) =>

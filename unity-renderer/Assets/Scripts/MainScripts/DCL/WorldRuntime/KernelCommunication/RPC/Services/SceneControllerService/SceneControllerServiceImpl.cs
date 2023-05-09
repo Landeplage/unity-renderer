762c08b82a79bae57e22c470c442f0f794281829
Lean Mendoza <leandro@decentraland.org>
2023-05-09 08:46:54 -0300

    fix raycast

--- unity-renderer/Assets/Scripts/MainScripts/DCL/WorldRuntime/KernelCommunication/RPC/Services/SceneControllerService/SceneControllerServiceImpl.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/WorldRuntime/KernelCommunication/RPC/Services/SceneControllerService/SceneControllerServiceImpl.cs
@@ -377,7 +377,7 @@ namespace RPC.Services
 
             return new RaycastQuery
             {
-                id = action.Payload.Query.QueryId,
+                id = action.Payload.Query.Payload.QueryId,
                 raycastType = raycastType,
                 ray = ray,
                 sceneNumber = sceneNumber,
